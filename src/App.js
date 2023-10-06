34e2e8444f0ff50b89f6db97d6a90352e9be808e
prathyu1929 <prathuprathu496@gmail.com>
2023-10-05 22:00:41 -0500

    React

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import 'bulma/css/bulma.css';
+import ProfileCard from './ProfileCard';
+import AlexaImage from './Images/alexa.png';
+import CortanaImage from './Images/cortana.png';
+import SiriImage from './Images/siri.png';
+
+
+
+function App() {
+  return( 
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      
+      
+      <div className="container">
+        <div className="columns">
+          <div className="column is-4">
+          <ProfileCard title="Alexa" 
+          handle="@alxa99" 
+          image={AlexaImage}
+          description="Alexa was created by Amazon and helps you buy things."/>
+          </div>
+          <div className="column is-4">
+          <ProfileCard title="Cortana" 
+          handle="@Cortana32" 
+          image={CortanaImage}
+          description="Cortana was made by Microsoft. Who knows what it does?"/>
+          </div>
+          <div className="column is-4">
+          <ProfileCard title="Siri" 
+          handle="@Siri01" 
+          image={SiriImage}
+          description="Siri was made by Apple and is being phased out"/>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  
+  );
+}
+export default App;
+
